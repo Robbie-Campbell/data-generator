7935179e61a3d533755d54d7c817cabe4035a0f2
robbie <robbielcampbell@hotmail.com>
2020-11-11 17:33:59 +0000

    :sparkles: foreign key referencing :sparkles:

--- src/NoDB.java
+++ src/NoDB.java
@@ -3,13 +3,14 @@ I realise now i am trying to achieve too many things at once, this class will
 just create statements, without trying to link to a mysql db, that can come later.
  */
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class NoDB {
 
     private final String[] firstNameList, lastNameList, addressLine1, roadTitle, addressLine2;
-    private final String PKName;
+    private String PKName;
     private int index = 0;
     private ArrayList<String> values;
 
@@ -33,9 +34,6 @@ public class NoDB {
         this.roadTitle = new String[]{"Provost", "Drive", "Close", "Road", "Avenue", "Street", "Gardens", "Grove", "Lane"};
 
         this.addressLine2 = new String[]{"Dorset", "Somerset", "Heartfordshire", "Essex"};
-
-        // Create a sample variable
-        this.PKName = "Academic_Staff";
     }
 
     // Create a random email
@@ -75,7 +73,7 @@ public class NoDB {
     // Create all primary keys values
     public String getIDName()
     {
-        String[] pkLetters = PKName.split("_");
+        String[] pkLetters = this.PKName.split("_");
         String id;
         if (pkLetters.length > 1)
             id = String.valueOf(pkLetters[0].charAt(0)) + pkLetters[1].charAt(0);
@@ -110,35 +108,51 @@ public class NoDB {
     }
 
     // Creates a table with listed parameters
-    public String createTable(String tableName, Boolean name, Boolean personalInfo)
+    public String createTable(String tableName, String fk, Boolean name, Boolean personalInfo)
     {
         StringBuilder statement = new StringBuilder(String.format("CREATE TABLE %s (%s", tableName.toLowerCase(),
-                tableName.toLowerCase() + "_id VARCHAR(7) PRIMARY KEY NOT NULL,\n"));
+                tableName.toLowerCase() + "_id VARCHAR(7) PRIMARY KEY NOT NULL"));
+        if (!fk.equals(""))
+        {
+            statement.append(",\n").append(fk).append("_id VARCHAR(7) NOT NULL");
+        }
         if (name)
         {
-            statement.append("first_name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, email VARCHAR(50) NOT NULL,\n");
+            statement.append(",\nfirst_name VARCHAR(20) NOT NULL,\nlast_name VARCHAR(20) NOT NULL,\nemail VARCHAR(50) NOT NULL");
         }
         if (personalInfo)
         {
-            statement.append("contact_no VARCHAR(11) NOT NULL, DOB DATE NOT NULL, address_line_1 VARCHAR(50) NOT NULL, \n" +
-                    "address_line_2 VARCHAR(50) NOT NULL, postcode VARCHAR(9) NOT NULL");
+            statement.append(",\ncontact_no VARCHAR(11) NOT NULL,\nDOB DATE NOT NULL,\naddress_line_1 VARCHAR(50) NOT NULL,\n" +
+                    "address_line_2 VARCHAR(50) NOT NULL,\npostcode VARCHAR(9) NOT NULL");
+        }
+        if (!fk.equals(""))
+        {
+            statement.append(String.format(",\nFOREIGN KEY (%s_id) references %s(%s_id)", fk, fk, fk));
         }
         statement.append(");");
         return statement.toString();
     }
 
     // Execute the program, pass in params to decide which parts of the statement you want to include
-    public void insertStatements(String tableTitle, Boolean name, Boolean personalInfo)
+    public void insertStatements(String tableTitle, String fk, Boolean name, Boolean personalInfo)
     {
-        System.out.println(this.createTable(tableTitle, name, personalInfo));
-        for (int i = 0; i < 5; i++)
+        this.createTable(tableTitle);
+        this.PKName = tableTitle;
+        int numberOfRows = 5;
+        System.out.println(this.createTable(tableTitle, fk, name, personalInfo));
+        for (int i = 0; i < numberOfRows; i++)
         {
             this.index++;
+            String id = this.generateKey();
             String firstName = this.getFirstName();
             String lastName = this.getLastName();
             String email = generateEmail(firstName, lastName, this.generateKey());
             String insertStatement = String.format("INSERT INTO %s VALUES ('%s'", tableTitle,
-                    this.generateKey());
+                    id);
+            if (!fk.equals(""))
+            {
+                insertStatement += ", '" + this.getAllFKIDs(fk).get(i) + "'";
+            }
             if (name)
             {
                 insertStatement += String.format(", '%s', '%s', '%s'", firstName, lastName, email);
@@ -150,12 +164,61 @@ public class NoDB {
             }
             insertStatement += ");";
             System.out.println(insertStatement);
+            commitPKToDB(tableTitle, id);
         }
+    }
+
+    // Creates the table for all of the primary keys of this type
+    public void createTable (String tableName) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/data-generator", "root", "");
+            String createTable = String.format("CREATE TABLE %s (keyList VARCHAR(7) PRIMARY KEY);", tableName);
+            PreparedStatement stat = conn.prepareStatement(createTable);
+            stat.executeUpdate(createTable);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    // This saves all of the primary keys generated in the create table function so they can be referenced as foreign keys
+    // in other tables
+    public void commitPKToDB(String tableName, String primaryKey)
+    {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/data-generator", "root", "");
+            PreparedStatement stat = conn.prepareStatement(String.format("INSERT INTO %s VALUES(?)", tableName));
+            stat.setString(1, primaryKey);
+            stat.executeUpdate();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    // Retrieves all of the primary keys from a given table, this streamlines the production of foreign keys
+    public ArrayList<String> getAllFKIDs(String tableName)
+    {
+        ArrayList<String> allIds = new ArrayList<>();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/data-generator", "root", "");
+            PreparedStatement stat = conn.prepareStatement(String.format("SELECT * FROM %s", tableName));
+            stat.executeQuery();
+            ResultSet rs = stat.getResultSet();
+            while (rs.next())
+            {
+                allIds.add(rs.getString("keyList"));
+            }
+            conn.close();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allIds;
     }
 
     public static void main(String[] args)
     {
         NoDB start = new NoDB();
-        start.insertStatements("Student", false, true);
+        start.insertStatements("Staff_Full", "Student", true, true);
     }
 }

d4b6ea5a5cf616ae9a3af14e779cefdbc6460325
robbie <robbielcampbell@hotmail.com>
2020-11-16 11:31:50 +0000

    :art: neatened code slightly :art:

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -6,15 +6,13 @@ just create statements, without trying to link to a mysql db, that can come late
 import java.sql.*;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class DataGenerator {
 
     private final String[] firstNameList, lastNameList, addressLine1, roadTitle, addressLine2;
     private String PKName;
     private static int index;
-    boolean createTable = true;
-    private ArrayList<String> values;
+    int current = 0;
 
     public DataGenerator() {
 
@@ -52,10 +50,13 @@ public class DataGenerator {
     }
 
     // Generate a random date value with a lowest value for day month and year
-    public String generateDate(int minYear, int minMonth, int minDay)
+    public String generateDate(int minYear, int minMonth, int minDay, int... maximum)
     {
-        return  (int) (minYear + Math.random() * (2020 - minYear)) + "-" + (int) (minMonth + Math.random() * (12 - minMonth)) + "-" +
-                (int) (minDay + Math.random() * (28 - minDay));
+        int maximumYear = maximum.length == 1 ? maximum[0] : 2020 - minYear;
+        int maximumMonth = maximum.length == 2 ? maximum[1] : 12 - minMonth;
+        int maximumDay = maximum.length == 3 ? maximum[2] : 12 - minDay;
+        return  (int) (minYear + Math.random() * maximumYear) + "-" + (int) (minMonth + Math.random() * maximumMonth) + "-" +
+                (int) (minDay + Math.random() * maximumDay);
     }
 
     // User key function
@@ -121,7 +122,7 @@ public class DataGenerator {
     }
 
     // Creates a table with listed parameters
-    public String createTable(String tableName, boolean pk, String[] fk, Boolean name, Boolean personalInfo, ExtraColumns[] moreValues)
+    public String generateTable(String tableName, boolean pk, String[] fk, Boolean name, Boolean personalInfo, ExtraColumn[] moreValues)
     {
         StringBuilder statement = new StringBuilder(String.format("CREATE TABLE %s (", tableName.toLowerCase()));
         if (pk)
@@ -144,7 +145,7 @@ public class DataGenerator {
         }
         if (moreValues.length > 0)
         {
-            for (ExtraColumns value: moreValues)
+            for (ExtraColumn value: moreValues)
             {
                 if (value.getDataType().equals("DATE"))
                     statement.append(String.format("%s %s %s %s,\n", value.getColumnName(), value.getDataType(), value.getDataSize(), value.getNullValue()));
@@ -160,6 +161,7 @@ public class DataGenerator {
         }
         statement.setLength(statement.length() - 2);
         statement.append("\n);");
+        commitTableToDatabase(tableName);
         return statement.toString();
     }
 
@@ -183,15 +185,17 @@ public class DataGenerator {
         return 0;
     }
 
+    // Enter a set of values and a random value with be returned
+    public String getRandomRole(String[] roles)
+    {
+        return roles[(int) (Math.random() * roles.length)];
+    }
+
     // Execute the program, pass in params to decide which parts of the statement you want to include, the default FK
     // value should be an empty string array thus returning no fks
-    public void insertStatements(String tableTitle, boolean pk, String[] fk, Boolean nameAndEmail, Boolean personalInfo, ExtraColumns[] moreValues)
+    public void insertStatements(String tableTitle, boolean pk, String[] fk, Boolean nameAndEmail, Boolean personalInfo, ExtraColumn[] moreValues)
     {
-        if (createTable) {
-            this.createTable(tableTitle);
-            this.PKName = tableTitle;
-            System.out.println(this.createTable(tableTitle, pk, fk, nameAndEmail, personalInfo, moreValues));
-        }
+        this.PKName = tableTitle;
         index = this.getCurrentIndex();
         String id = this.generateKey();
         String firstName = this.getFirstName();
@@ -203,11 +207,10 @@ public class DataGenerator {
         }
         if (fk.length > 0)
         {
-            int current = 0;
             for (String key: fk) {
-                current++;
-                insertStatement.append("'").append(this.getAllFKIDs(key).get(current)).append("',");
+                insertStatement.append("'").append(this.getAllFKIDs(key).get(this.current)).append("',");
             }
+            this.current ++;
         }
         if (nameAndEmail)
         {
@@ -220,14 +223,16 @@ public class DataGenerator {
         }
         if (moreValues.length > 0)
         {
-            for (ExtraColumns value: moreValues)
+            for (ExtraColumn value: moreValues)
             {
                 switch (value.getDataType()) {
                     case "VARCHAR":
+                    case "TEXT":
                         insertStatement.append(String.format("'%s', ", value.getInsertDataType()));
                         break;
                     case "INT":
                     case "DECIMAL":
+                    case "TINYINT":
                         insertStatement.append(value.getInsertDataType()).append(", ");
                         break;
                     case "DATE":
@@ -242,12 +247,10 @@ public class DataGenerator {
         if (pk) {
             commitPKToDB(tableTitle, id);
         }
-
-        createTable = false;
     }
 
     // Creates the table for all of the primary keys of this type.
-    public void createTable (String tableName) {
+    public void commitTableToDatabase (String tableName) {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/data-generator", "root", "");
             String createTable = String.format("CREATE TABLE %s (keyList VARCHAR(8) PRIMARY KEY);", tableName);
@@ -299,13 +302,120 @@ public class DataGenerator {
     public static void main(String[] args)
     {
         DataGenerator start = new DataGenerator();
-        int numberOfRows = 5;
 
-        // Redefine the numberOfRows variables to generate more or less data
-        for (int i = 0; i < numberOfRows; i++) {
-            start.insertStatements("Student", true, new String[]{}, true, true,
-                    new ExtraColumns[]{new ExtraColumns("date_of_start", "DATE", "", false, start.generateDate(2010, 1, 1)),
-                            new ExtraColumns("income", "DECIMAL", "7,2", true, start.generateMoneyValue(1000, 5000))});
+        //////////////////////////////////////////// CREATE ALL OF THE TABLES //////////////////////////////////////////
+
+        // Staff table
+        System.out.println(start.generateTable("Staff", true, new String[]{},  true, true,
+                new ExtraColumn[]{
+                new ExtraColumn("role", "VARCHAR", "10", false),
+                new ExtraColumn("gross_salary_pm", "DECIMAL", "6,2", false),
+                new ExtraColumn("is_absent", "TINYINT", "1", false)}));
+
+        // Department table
+        System.out.println(start.generateTable("Department", true, new String[]{}, false, false,
+                new ExtraColumn[]{
+                new ExtraColumn("department_name", "VARCHAR", "10", false ),
+                new ExtraColumn("department_salary_pa", "DECIMAL", "7,2", false)}));
+
+        // Non academic table
+        System.out.println(start.generateTable("Non_Academic", false, new String[]{"Staff", "Department"}, false, false,
+                new ExtraColumn[]{
+                new ExtraColumn("skill_bonus", "DECIMAL", "6,2", true)}));
+
+        // Staff income table
+        System.out.println(start.generateTable("Staff_Wage", false, new String[]{"Staff"}, false, false,
+                new ExtraColumn[]{
+                new ExtraColumn("month", "DATE", "", false),
+                new ExtraColumn("net_month_salary", "DECIMAL", "7,2", false),
+                new ExtraColumn("absence_deduction", "DECIMAL", "6,2", false),
+                new ExtraColumn("tax_for_month", "DECIMAL", "6,2", false),
+                new ExtraColumn("meeting_bonus", "DECIMAL", "6,2", false)}));
+
+        // Staff Absence table
+        System.out.println(start.generateTable("Absence", false, new String[]{"Staff"}, false, false, new
+                ExtraColumn[]{
+                new ExtraColumn("date_of_leave", "DATE", "", false, start.generateDate(2019, 4, 1, 2020, 1, 3)),
+                new ExtraColumn("date_of_return", "DATE", "", false, start.generateDate(2019, 4, 1, 2019, 1,3 )),
+                new ExtraColumn("duration_of_absence", "INT", "2", false, "0"),
+                new ExtraColumn("details_of_absence", "VARCHAR", "255", false, "Lorem Ipsum Dolor Dest")}));
+
+        ///////////////////////////////////////////////// GENERATE ALL DATA ////////////////////////////////////////////
+
+        // Generate staff data
+        for (int i = 0; i < 5; i++) {
+            start.insertStatements("Staff", true, new String[]{},  true, true,
+                    new ExtraColumn[]{
+                    new ExtraColumn("role", "VARCHAR", "10", false, start.getRandomRole(new String[]{"HOD", "Teacher"})),
+                    new ExtraColumn("gross_salary_pm", "DECIMAL", "6,2", false, start.generateMoneyValue(200, 1000)),
+                    new ExtraColumn("is_absent", "TINYINT", "1", false, "0")});
+        }
+
+        // Generate department data
+        for (int i = 0; i < 5; i++) {
+            start.insertStatements("Department", true, new String[]{}, false, false,
+                    new ExtraColumn[]{
+                    new ExtraColumn("department_name", "VARCHAR", "10", false, "Admin"),
+                    new ExtraColumn("department_salary_pa", "DECIMAL", "7,2", false, start.generateMoneyValue(20000, 40000))});
+        }
+
+        // Generate non academic data data
+        for (int i = 0; i < 5; i++) {
+            start.insertStatements("Non_Academic", false, new String[]{"Staff", "Department"}, false, false,
+                    new ExtraColumn[]{
+                    new ExtraColumn("skill_bonus", "DECIMAL", "6,2", true, start.generateMoneyValue(0,120))});
+        }
+
+        // Generate absence data
+        start.current = 0;
+        for (int i = 0; i < 5; i++){
+            start.insertStatements("Absence", false, new String[]{"Staff"}, false, false,
+                    new ExtraColumn[]{
+                    new ExtraColumn("date_of_leave", "DATE", "", false, start.generateDate(2019, 4, 1, 2020, 1, 3)),
+                    new ExtraColumn("date_of_return", "DATE", "", false, start.generateDate(2019, 4, 1, 2019, 1,3 )),
+                    new ExtraColumn("duration_of_absence", "INT", "2", false, String.valueOf((int) (Math.random() * 5))),
+                    new ExtraColumn("details_of_absence", "VARCHAR", "255", false, "Lorem Ipsum Dolor Dest")});
         }
     }
 }
+
+/*
+DROP TRIGGER create_staff_pay;
+CREATE TRIGGER create_staff_pay
+AFTER INSERT ON staff
+FOR EACH ROW
+INSERT
+	staff_wage
+SET
+	staff_wage.staff_id = NEW.staff_id,
+    staff_wage.month = CURDATE(),
+    staff_wage.absence_deduction = 40.00,
+    staff_wage.tax_for_month = NEW.gross_salary_pm * 0.2,
+    staff_wage.meeting_bonus = (SELECT COUNT(*) from staff) * 5,
+    staff_wage.net_month_salary = (NEW.gross_salary_pm + staff_wage.meeting_bonus) - staff_wage.tax_for_month - staff_wage.absence_deduction;
+
+SELECT * FROM staff_wage;
+*/
+
+/*
+DROP TRIGGER update_absence_deduction;
+CREATE TRIGGER update_absence_deduction
+AFTER INSERT ON absence
+FOR EACH ROW
+UPDATE
+	staff_wage
+SET
+	staff_wage.absence_deduction = staff_wage.absence_deduction + 5
+WHERE
+	staff_wage.staff_id = NEW.staff_id
+AND
+	NEW.duration_of_absence > 2;
+ */
+
+/*
+INSERT INTO Staff VALUES ('STA00001','Kevin', 'Kepler', 'kevinkeplersta00002@email.com', '07100273682', DATE '2012-10-21', 'Lincoln Drive', 'Dorset', 'LI15 3NC', 'HOD', 659.41, 0);
+INSERT INTO Staff VALUES ('STA00002','Nina', 'Waterson', 'ninawatersonsta00003@email.com', '07118743393', DATE '2010-7-6', 'Water Lane', 'Heartfordshire', 'WA4 11TE', 'HOD', 209.14, 0);
+INSERT INTO Staff VALUES ('STA00003','Lebron', 'Granada', 'lebrongranadasta00004@email.com', '07137697265', DATE '1977-5-2', 'Ipsum Avenue', 'Somerset', 'IP9 26SU', 'HOD', 817.87, 0);
+INSERT INTO Staff VALUES ('STA00004','Natalie', 'Peterson', 'nataliepetersonsta00005@email.com', '07167860819', DATE '2005-9-10', 'Cribbage Grove', 'Dorset', 'CR19 8IB', 'HOD', 342.30, 0);
+INSERT INTO Staff VALUES ('STA00005','Craig', 'Granada', 'craiggranadasta00006@email.com', '07194730201', DATE '1964-3-1', 'Fishbrook Avenue', 'Somerset', 'FI28 25SH', 'Headmaster', 1113.74, 0);
+ */
--- src/ExtraColumn.java
+++ src/ExtraColumn.java
@@ -1,6 +1,7 @@
+import java.util.Optional;
 
 // Return a new column for a table and create new data for it
-public class ExtraColumns {
+public class ExtraColumn {
 
     private final String columnName;
     private final String dataType;
@@ -8,12 +9,12 @@ public class ExtraColumns {
     private final String insertDataType;
     private final String nullValue;
 
-    public ExtraColumns(String columnName, String dataType, String dataSize, boolean nullable, String insertDataType)
+    public ExtraColumn(String columnName, String dataType, String dataSize, boolean nullable, String... insertDataType)
     {
         this.columnName = columnName;
         this.dataType = dataType;
         this.dataSize = dataSize;
-        this.insertDataType = insertDataType;
+        this.insertDataType = insertDataType.length > 0 ? insertDataType[0] : "";
         if (!nullable)
         {
             this.nullValue = "NOT NULL";
@@ -41,7 +42,5 @@ public class ExtraColumns {
         return this.nullValue;
     }
 
-    public String getInsertDataType() {
-        return this.insertDataType;
-    }
+    public String getInsertDataType() { return this.insertDataType; }
 }
